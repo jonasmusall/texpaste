260f6c289acea2f0261d3fc54e2484982b991dbc
jonasmusall <jonas.musall@gmail.com>
2021-07-18 15:12:34 +0200

    Create main window

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const { app, BrowserWindow } = require("electron");
 const katex = require("katex");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 300,
+        frame: false,
+        resizable: false
+    });
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
