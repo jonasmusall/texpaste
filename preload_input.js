b0c8d0d6813ee7a7bfc132810aebf73a251af63d
jonasmusall <jonas.musall@gmail.com>
2021-07-21 09:48:24 +0200

    Move code from input.js to preload script

--- preload_input.js
+++ preload_input.js
@@ -1,4 +1,5 @@
 const { ipcRenderer } = require("electron");
+const katex = require("katex");
 
 let input, output;
 
@@ -11,11 +12,45 @@ function sizeChanged() {
 
 function updateTex() {
     ipcRenderer.send("tex", input.value);
+    katex.render(
+        input.value,
+        output,
+        {
+            displayMode: true,
+            output: "html",
+            throwOnError: false,
+            strict: "ignore"
+        }
+    )
+}
+
+function accept() {
+    navigator.clipboard.writeText(input.value).then(
+        () => window.close(),
+        () => window.close()
+    )
+}
+
+function cancel() {
+    window.close();
+}
+
+function handleKeyUp(event) {
+    if (event.key == "Enter") {
+        accept();
+    } else if (event.key == "Escape") {
+        cancel();
+    }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
     input = document.getElementById("tex-input");
     output = document.getElementById("tex-output");
     input.addEventListener("input", updateTex);
+    input.addEventListener("keyup", handleKeyUp);
+    document.getElementById("accept").addEventListener("click", accept);
+    document.getElementById("cancel").addEventListener("click", cancel);
     new ResizeObserver(sizeChanged).observe(output);
+    document.body.addEventListener("click", () => input.focus());
+    input.focus();
 });
